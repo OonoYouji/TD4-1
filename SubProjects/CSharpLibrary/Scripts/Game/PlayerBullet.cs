89c802596f1c056be7851486e4f0a27d98e9f36d
OonoYouji <k023g0007@g.neec.ac.jp>
2026-05-04 12:05:50 +0900

    プレイヤーの弾のprefabを作成 プレイヤーの弾の

---
+++ SubProjects/CSharpLibrary/Scripts/Game/PlayerBullet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PlayerBullet : MonoScript {
+
+	[SerializeField]
+	float speed = 0.1f;
+
+	[SerializeField]
+	float lifeTime = 5.0f;
+
+	public override void Initialize() {
+		Debug.Log("PlayerBullet Start");
+	}
+
+	public override void Update() {
+		Debug.Log("PlayerBullet Update");
+		transform.position += new Vector3(0, 0, speed);
+
+		lifeTime -= Time.deltaTime;
+
+		if (lifeTime <= 0) {
+			entity.Destroy();
+		}
+	}
+
+}
---
+++ SubProjects/CSharpLibrary/Scripts/Game/PlayerBulletLauncher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PlayerBulletLauncher : MonoScript {
+
+	public override void Initialize() {
+		Debug.Log("PlayerBulletLauncher Start");
+	}
+
+	public override void Update() {
+		Debug.Log("PlayerBulletLauncher Update");
+		if (Input.TriggerKey(KeyCode.Space)) {
+			var bullet = ecsGroup.CreateEntity("PlayerBullet");
+			bullet.transform.position = transform.position;
+		}
+	}
+
+}
