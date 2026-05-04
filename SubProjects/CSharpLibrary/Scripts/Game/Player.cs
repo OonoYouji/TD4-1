e34648748b44e1c954fa49b25ac3d4a23cfdb92a
OonoYouji <k023g0007@g.neec.ac.jp>
2026-05-04 10:35:51 +0900

    Playern

--- SubProjects/CSharpLibrary/Scripts/Game/Player.cs
+++ SubProjects/CSharpLibrary/Scripts/Game/Player.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 public class Player : MonoScript {
 
+	float rotateY = 0f;
 
+	public override void Initialize() {
+		Debug.Log("Player Start");
+	}
+
+	public override void Update() {
+		Debug.Log("Player Update");
+
+		rotateY += 1.0f;
+
+		transform.rotate = Quaternion.FromEuler(new Vector3(0, rotateY, 0));
+
+	}
 
 }
