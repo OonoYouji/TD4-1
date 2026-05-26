4e895533c2ead7d968d5758f7d27d227d6aa71ca
OonoYouji <k023g0007@g.neec.ac.jp>
2026-05-27 01:08:43 +0900

    feature boss ui

--- Project/Engine/ECS/System/Collision/CollisionSystem.cpp
+++ Project/Engine/ECS/System/Collision/CollisionSystem.cpp
@@ -10,6 +10,7 @@ using namespace ONEngine;
 #include "Engine/Core/Utility/Utility.h"
 #include "Engine/ECS/EntityComponentSystem/EntityComponentSystem.h"
 #include "Engine/Script/MonoScriptEngine.h"
+#include "Engine/Core/Utility/Time/CPUTimeStamp.h"
 #include "Engine/ECS/Component/Components/ComputeComponents/Collision/BoxCollider.h"
 #include "Engine/ECS/Component/Components/ComputeComponents/Collision/SphereCollider.h"
 #include "Engine/ECS/Component/Components/ComputeComponents/Collision/CollisionCheck/CollisionCheck.h"
@@ -67,6 +68,7 @@ CollisionSystem::CollisionSystem() {
 
 
 void CollisionSystem::RuntimeUpdate(ECSGroup* _ecs) {
+	CPUTimeStamp::GetInstance().BeginTimeStamp(CPUTimeStampID::PhysicsUpdate);
 
 	enterPairs_.clear();
 	stayPairs_.clear();
@@ -220,6 +222,7 @@ void CollisionSystem::RuntimeUpdate(ECSGroup* _ecs) {
 	CallStayFunc(ecsGroupName);
 	CallExitFunc(ecsGroupName);
 
+	CPUTimeStamp::GetInstance().EndTimeStamp(CPUTimeStampID::PhysicsUpdate);
 }
 
 void CollisionSystem::CallEnterFunc(const std::string& _ecsGroupName) {
